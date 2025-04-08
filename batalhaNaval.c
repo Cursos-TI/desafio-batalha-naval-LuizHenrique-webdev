bafb92ed0d4e10cabab6e903e17af542538ad284
LuizHenrique-webdev <almeidaluiz578@gmail.com>
2025-04-08 19:27:27 +0000

    iniciei o desafio Batalha Naval e implementei matrizes  para tabuleiro e vetores para navios - nivel novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,36 +5,48 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+        int tabuleiro[10][10]={                         //declaração de matriz 10x10, inicialmente todos os valores da matriz são 0
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        char letrasbatalha[10] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'              //declaração  de cabecalho de letras para mapeamento e orientação usuario
+        };
+        char *colunabatalha[10] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10"   //declaração de cabecalho de numeros para mapeamento e orientação do usuario
+        };
+        int navio[3] = {3, 3, 3};       //declaração do vetor do primeiro navio
+        int navio2[3] = {3, 3, 3};      //declaração do vetor do segundo navio
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+        tabuleiro[2][2]=navio[0];       //posicionando o primeiro navio 
+        tabuleiro[2][3]=navio[1];       //as linhas do tabuleiro se mantém para a orientação horizontal
+        tabuleiro[2][4]=navio[2];       //cada coordenada do tabuleiro recebe um valor do navio
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+        tabuleiro[4][7]=navio2[0];      //posicionando o segundo navio
+        tabuleiro[5][7]=navio2[1];      //as colunas do tabuleiro se mantém para a orientação vertical
+        tabuleiro[6][7]=navio2[2];      //cada coordenada do tabuleiro recebe um valor do segundo navio
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+        printf("             JOGO DE BATALHA NAVAL\n");    //cabeçalho  de jogo
+        printf(" \n");                                     //pula uma linha
+        printf("  ");                                      //espaço  impresso para posicionar melhor o cabeçalho de letras
+        for (int i=0; i<10; i++){                          //loop para imprimir todos os valores do cabelhado de letras
+            printf(" %c", letrasbatalha[i]);               
+        }
+        printf("\n");                              //pula uma linha no console
+        for(int j=0; j<10; j++){                   //loop para imprimir colunas do cabeçalho de numeros 
+            printf("%s", colunabatalha[j]); 
+            for (int k=0; k<10; k++){              //loop para imprimir todas as linhas da coluna do primeiro loop
+                printf(" %d", tabuleiro[j][k]);
+            }
+        printf("\n");                              //pula uma linha para o efeito de tabela //a linha deve pular depois que segundo loop terminar de preencher todas as linhas de uma coluna  
+        }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+
 
     return 0;
 }
