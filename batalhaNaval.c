27999477f7ac622c360ad03ed1253c838430a1a9
LuizHenrique-webdev <almeidaluiz578@gmail.com>
2025-04-09 15:45:45 +0000

    navio na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -20,22 +20,50 @@ int main() {
         };
         char *colunabatalha[10] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10"   //declaração de cabecalho de numeros para mapeamento e orientação do usuario
         };
+        int index=0;
+
         int navio[3] = {3, 3, 3};       //declaração do vetor do primeiro navio
         int navio2[3] = {3, 3, 3};      //declaração do vetor do segundo navio
+        int navio3[3] = {3, 3, 3};      //declaração do vetor do terceiro navio
+        int navio4[4] = {3, 3, 3};      //declaração do vetor do quarto navio
 
-        tabuleiro[2][2]=navio[0];       //posicionando o primeiro navio 
-        tabuleiro[2][3]=navio[1];       //as linhas do tabuleiro se mantém para a orientação horizontal
-        tabuleiro[2][4]=navio[2];       //cada coordenada do tabuleiro recebe um valor do navio
+        index=2;
 
-        tabuleiro[4][7]=navio2[0];      //posicionando o segundo navio
-        tabuleiro[5][7]=navio2[1];      //as colunas do tabuleiro se mantém para a orientação vertical
-        tabuleiro[6][7]=navio2[2];      //cada coordenada do tabuleiro recebe um valor do segundo navio
+        for (int k=0;  k<3; k++){
+            if (k>=0 && k<=9){
+             tabuleiro[index][k]=navio[k];
+            }else{
+                printf("essa posição esta fora do tabuleiro\n");}
+        }
+
+        index=7;
+
+        for(int l=0; l<3; l++){
+            if (l>=0 && l<=9){
+                tabuleiro[l][index]=navio2[l];
+            }else{
+                printf("essa posição esta fora do tabuleiro\n");
+            }
+        }
+
+        for (int m=0; m<3; m++){
+            if(m>=0 && m<=9){
+                for(int n=0; n<3; n++){
+                    if(n>=0 && n<=9){
+                        if(m==n){
+                            tabuleiro[m][n]=navio3[n];
+                        }
+                    }
+                }
+            }
+        }
+        
 
         printf("             JOGO DE BATALHA NAVAL\n");    //cabeçalho  de jogo
         printf(" \n");                                     //pula uma linha
         printf("  ");                                      //espaço  impresso para posicionar melhor o cabeçalho de letras
         for (int i=0; i<10; i++){                          //loop para imprimir todos os valores do cabelhado de letras
-            printf(" %c", letrasbatalha[i]);               
+            printf(" %c", letrasbatalha[i]);            
         }
         printf("\n");                              //pula uma linha no console
         for(int j=0; j<10; j++){                   //loop para imprimir colunas do cabeçalho de numeros 
